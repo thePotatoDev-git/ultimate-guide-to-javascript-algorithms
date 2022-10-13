27f8fe4bea43ce11e37eb819a4e9250acbd28872
Albertus Chua <albertus.chua@gmail.com>
2022-10-13 13:14:24 -0400

    reverseString and vowelsCounter completed

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,50 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
+////////////// Chaining built-in methods
 
 function reverseString(text) {
     // Code goes here
+    return text.split('').reverse().join('');
 }
 
+/////////////// For-loop way
 
+function reverseString(text) {
+    let result = '';
+
+    for (let i = text.length - 1; i >= 0; i--) {
+        result += text[i];
+    }
+    return result;
+}
+
+/////////////// For-of loop way
+
+function reverseString(text) {
+    let result = '';
+
+    for (let char of text) {
+        result = char + result;
+    }
+    return result;
+}
+
+////////////// Recursion way
+
+function reverseString(text) {
+// Terminal case: The case that ends the recursion. Checks if the string is empty and ends it immediately if it is. Without his, the process would continue in an endless loop.
+    if (text === '') {
+        return '';      
+    } else {
+        return reverseString(text.substr(1)) + text[0];
+    }
+}
+
+// Reduce method
+
+function reverseString(text) {
+    return text.split('').reduce((acc, char) => char + acc, '');
+}
 
 module.exports = reverseString
--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,11 +3,30 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
+////////// Iterative approach
 
 function vowelsCounter(text) {
-    // Code goes here
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let vowelCounter = 0;
+
+    for (let i = 0; i < text.length; i++) {
+        if (vowels.includes(text[i].toLowerCase())) {
+            vowelCounter++;
+        }
+    }
+    return vowelCounter;
 }
 
+/////////// Regex
 
+function vowelsCounter(text) {
+    let matchingInstances = text.match(/[aeiou]/gi);
+
+    if (matchingInstances) {
+        return matchingInstances.length;
+    } else {
+        return 0;
+    }
+}
 
 module.exports = vowelsCounter;
