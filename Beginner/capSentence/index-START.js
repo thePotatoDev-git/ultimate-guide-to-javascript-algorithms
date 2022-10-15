7e917e2a7feaf7cbdebe8266e924813f378c939d
Albertus Chua <albertus.chua@gmail.com>
2022-10-14 21:30:46 -0400

    Completed maxRecurringChar and capSentence

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -5,12 +5,45 @@ return the equivalent of the sentence when capitalised. E.g
 */
 
 
-
-
+// Map and substring method
 function capSentence(text) {
-   // Code goes here
+  text = text.split(' ');
+
+  return text.map(word => word[0].toUpperCase() + word.substring(1)).join(' ');
 }
 
+// For each method
+function capSentence(text) {
+   let wordsArray = text.toLowerCase().split(' ');
+   let capsArray = [];
+
+   wordsArray.forEach(word => {
+    capsArray.push(word[0].toUpperCase() + word.slice(1));
+   });
+
+   return capsArray.join(' ');
+}
+
+// .map and .slice
+function capSentence(text) {
+  let wordsArray = text.toLowerCase().split(' ');
+  let capsArray = wordsArray.map(word => {
+    return word[0].toUpperCase() + word.slice(1)
+  });
+
+  return capsArray.join(' ');
+}
+
+// .map and .replace
+function capSentence(text) {
+  let wordsArray = text.toLowerCase().split(' ');
+
+  let capsArray = wordsArray.map(word => {
+    return word.replace(word[0], word[0].toUpperCase())
+  });
+
+  return capsArray.join(' ');
+}
 
 
 module.exports = capSentence
--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -6,9 +6,59 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 
 
 function maxRecurringChar(text) {
-    // Code goes here
+    let charMap = {};
+
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1;
+        }
+    }
+    return charMap
 }
 
+function maxRecurringChar(text) {
+    let charMap = {};
+    let maxCharValue = 0;
+    let maxChar = '';
 
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+    for (let char in charMap) {
+        if (charMap[char] > maxCharValue) {
+            maxCharValue = charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
+
+}
+
+function maxRecurringChar(text) {
+    let charMap = {};
+    let charArray = [];
+    let valuesArray = [];
+    let maxCharValue = 0;
+
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    charArray = Object.keys(charMap);
+    valuesArray = Object.values(charMap);
+    maxCharValue = Math.max(...valuesArray);
+
+    return charArray[valuesArray.indexOf(maxCharValue)];
+}
 
 module.exports = maxRecurringChar;
